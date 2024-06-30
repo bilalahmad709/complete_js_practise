3c85cc3ad8bc8998e055d41dfb065686d57a2f4c
Bilal Ahmad <bilalmomand709@gmail.com>
2024-06-30 15:34:13 +0500

    worked in DOM

---
+++ inner/inner.js
@@ -0,0 +1,18 @@
+// targetting the text only using innertext 
+var paragraph=document.getElementById("paragraph").innerText;
+console.log(paragraph);
+
+// targetting the html using innerhtml
+
+var paragrap = document.getElementById("secondparagraph").innerHTML;
+console.log(paragrap);
+
+// get attributes
+
+var attribut = document.getElementById("secondparagraph").getAttribute("style");
+console.log(attribut);
+
+// get AttributeNode
+
+var attributeNode = document.getElementById("secondparagraph").getAttributeNode("class");
+console.log(attributeNode);
