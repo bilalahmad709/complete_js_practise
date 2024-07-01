c8d606af2700080767c9e8d798ff8b1bcd09047d
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-01 12:03:09 +0500

    worked in setAttribute

---
+++ Set-Attribute-DOM/Set-attribute.js
@@ -0,0 +1,19 @@
+   // Setting the inner HTML of the greeting element
+   var greeting = document.getElementById("greeting");
+   greeting.innerHTML = "Welcome to my channel";
+
+   // Changing the inner text of para1
+   var para1 = document.getElementById("para1");
+   para1.innerText = "The inner text has been changed";
+
+   // Setting a style attribute for the second paragraph
+   var secondpara = document.getElementById("secondpara");
+   secondpara.setAttribute("style", "border: 2px dotted blue");
+
+   // Function to modify the second paragraph
+   function modifySecondPara() {
+       var secondpara = document.getElementById("secondpara");
+       secondpara.style.fontSize = "20px";
+       secondpara.style.padding = "10px";
+       secondpara.style.backgroundColor = "yellow";
+   }
