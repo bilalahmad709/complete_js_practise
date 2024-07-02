9f820aade8a68d36dee5802b77f2d11cc5177bfb
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-02 12:27:12 +0500

    changing of images/ bulb on/off through js functions

---
+++ change-images/changeimg.js
@@ -0,0 +1,11 @@
+
+
+function turnonbulb(){
+    var img =document.getElementById("image");
+    img.src="bulbon.jpeg";
+
+}
+function turnoffbulb(){
+    var img = document.getElementById("image");
+    img.src="bulboff.jpeg";
+}
