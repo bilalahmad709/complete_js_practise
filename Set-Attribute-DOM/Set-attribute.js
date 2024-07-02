c6a9d29cade60fb116c1825dddb61148e7e7021c
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-02 10:44:05 +0500

    changed the style through javascript

--- Set-Attribute-DOM/Set-attribute.js
+++ Set-Attribute-DOM/Set-attribute.js
@@ -7,13 +7,15 @@
    para1.innerText = "The inner text has been changed";
 
    // Setting a style attribute for the second paragraph
-   var secondpara = document.getElementById("secondpara");
-   secondpara.setAttribute("style", "border: 2px dotted blue");
+   var secondpara = document.getElementById("secondparagraph");
+   secondpara.setAttribute("style", "border: 3px solid blue");
 
    // Function to modify the second paragraph
    function modifySecondPara() {
-       var secondpara = document.getElementById("secondpara");
+       var secondpara = document.getElementById("secondparagraph");
+       secondpara.style.border= "2px dotted blue";
        secondpara.style.fontSize = "20px";
        secondpara.style.padding = "10px";
        secondpara.style.backgroundColor = "yellow";
+       secondpara.style.fontFamily = "'Poppins', sans-serif";
    }
