956a3f4108e14a5da1edc56401c60379e91d6200
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-03 10:57:51 +0500

    worked on function, to change everything through js functions

---
+++ info-change/infochange.js
@@ -0,0 +1,43 @@
+function zahid(){
+    var div = document.getElementById("imgdiv");
+    div.innerHTML= `
+    <img src="zahid.jpg" alt=" zahid khattak" style=" width:150px; height:150px;">
+    <h2>Zahid khattak</h2>
+    <p>F/Name: Usman Gul </p>
+    <p>Email: zahidkhattackcs1233@gmail.com </p>
+    <p>Address : Qadari Banda, Tehsil Hango, District Hango </p>
+    `;
+}
+
+function umar(){
+    var div= document.getElementById("imgdiv");
+    div.innerHTML= `
+    <img src="umar.jpg" alt="umar hakim" style ="widht:150px; height:150px;">
+    <h2> Umar Hakim</h2>
+    <p>Mobile : 0325-67483838</p>
+    <p>Email : mrumar567@gmail.com</p>
+    <p>Address : Machlo Kanda Kombar, Tehsil Lal Qilla Maidaan Dir lower</p>
+    `;
+}
+
+function hilal(){
+    var div= document.getElementById("imgdiv");
+    div.innerHTML= `
+    <img src ="hilal.jpg" alt=" Hilal Azam" style="width:150px; height:150px;">
+    <h2>Hilal Azam</h2>
+    <p>Mobile :0305-912916</p>
+    <p>Email : hilal124@gmail.com</p>
+    <p>Adress : Village & Postoffice Haya Serai, Tehsil Temargara Dir Lower</p>
+    `;
+}
+
+function huzaifa(){
+    var div= document.getElementById("imgdiv");
+    div.innerHTML=  `
+    <img src="huzaifa.jpg" alt= " M Huzaifa" style="width:150px; height:150px;">
+    <h2>M Huzaifa</h2>
+    <p>Mobile : 0341-91912256</p>
+    <p>Email : mrhuzaifa125@gmail.com</p>
+    <p>Address : Village Ghazi beg, Tehsil Haleemzai, District Mohmand</p>
+    `
+}
