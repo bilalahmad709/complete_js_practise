23c46a8470f67fd503885b03e00810ff66c9f14f
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-04 11:59:09 +0500

    worked in Styling Dom methods ( style, className, classList)

--- querrySelector/querryselector.js
+++ querrySelector/querryselector.js
@@ -1,5 +1,13 @@
-var query= document.querySelector("#para");
-console.log(query);
+var paraElement= document.querySelector("#para");   
+ paraElement.innerHTML=`
+<h3> Welcome</h3>
+<p>This paragraph has been changed, using querselector</p>
+
+`;
+paraElement.style.border= "2px solid red";
+paraElement.style.padding= " 10px";
+paraElement.style.borderRadius= "10px";
+console.log(paraElement);
 
 var queryall= document.querySelectorAll(".para");
 console.log(queryall);
---
+++ styling/styling.js
@@ -0,0 +1,16 @@
+
+// styling method
+
+var firstPara= document.getElementById("firstPara").style.color="green";
+var firstPara= document.getElementById("firstPara").style.backgroundColor="tan";
+var firstPara= document.getElementById("firstPara").style.border=" 2px solid pink";
+var firstPara= document.getElementById("firstPara").style.padding=" 10px";
+var firstPara= document.getElementById("firstPara").style.borderRadius="10px";
+
+// className method
+
+var secondPara= document.getElementById("secondPara").className="abc";
+
+// classList method
+
+var thirdPara= document.getElementById("thirdPara").classList.add("efg");
