5dec0b3090d121921988198795ba4a3bc38ad2e4
Bilal Ahmad <bilalmomand709@gmail.com>
2024-07-10 00:23:01 +0500

    worked in traversal methods

--- traversal method/traversal.js
+++ traversal method/traversal.js
@@ -1,1 +1,7 @@
 var element= document.getElementById("inner1").parentElement.style.backgroundColor="lightgray";
+
+// Selecting children divs
+var childrenDiv = document.getElementById("inner2").children;
+childrenDiv[2].style.backgroundColor = "red"; 
+childrenDiv[4].style.backgroundColor = "red"; 
+childrenDiv[5].style.backgroundColor = "red"; 
